9d65e1c35a7716dd565943acd1258051868c0b54
Jamen Kaye <jamen.kaye@gmail.com>
2023-03-20 18:24:46 -0700

    Added image resizing; debug mode

--- rs_imaging.py
+++ rs_imaging.py
@@ -9,10 +9,9 @@ Full documentation for pyrealsense2 (its kinda bad tho):
     https://intelrealsense.github.io/librealsense/python_docs/_generated/pyrealsense2.html#module-pyrealsense2
 
 '''
+RESOLUTION = (128, 128)
 
 class RS_Imager:
-
-    RESOLUTION = (128, 128)
 
     def __init__(self):
         # Instantiate an RS_Imager object
@@ -24,20 +23,22 @@ class RS_Imager:
         self.pipeline.start(self.config)
 
 
-    def save_depth_image(self, file_ID, save_array=True, hole_filling_mode=1):
+    def save_depth_image(self, file_ID, save_array=True, debug_mode=False, hole_filling_mode=1):
         '''
             Purpose: Saves a depth image using the RealSense camera. Can optionally also save the image as an ndarray.
             Note: Assumes that these folders already exist:
                 ./Data/RS_Images/jpg
                 ./Data/RS_Images/npy
 
+            Arg - file_ID (string): this is the name of the file(s) to be saved. Do NOT include the file extension.
             Arg - save_array (boolean): determines if an ndarray should be saved as a .npy file as well as the .jpg
+            Arg - debug_mode (boolean): determines if full resolution images should be saved in addition to low-res.
             Arg - hole_filling_mode (int): determines if and how holes are filled. Options:
                         -1  No hole filling
                         0   Fill holes from the left
                         1   Fill holes with the deepest pixel that is nearby.
                             
-            Returns: an ndarray corresponding to the depth image
+            Returns: an ndarray corresponding to the depth image, in the reduced resolution.
         '''
 
         frames = self.pipeline.wait_for_frames()
@@ -48,15 +49,24 @@ class RS_Imager:
             hole_filler = rs.hole_filling_filter(hole_filling_mode)
             depth_frame = hole_filler.process(depth_frame)
 
-        depth_image = np.asanyarray(depth_frame.get_data())
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_RAINBOW)
+        full_res_array = np.asanyarray(depth_frame.get_data())
+        full_res_image = cv2.applyColorMap(cv2.convertScaleAbs(full_res_array, alpha=0.03), cv2.COLORMAP_RAINBOW)
 
-        cv2.imwrite("./Data/RS_Images/jpg/" + file_ID + ".jpg", depth_colormap)
+        low_res_image = cv2.resize(full_res_image, RESOLUTION)
+
+        cv2.imwrite("./Data/RS_Images/jpg/" + file_ID + ".jpg", low_res_image)
+
+        if debug_mode:
+            cv2.imwrite("./Data/RS_Images/jpg/" + file_ID + "-full_res.jpg", full_res_image)
         
         if save_array:
-            np.save("./Data/RS_Images/npy/" + file_ID + ".npy", depth_image)
+            low_res_array = cv2.resize(full_res_array, RESOLUTION)
+            np.save("./Data/RS_Images/npy/" + file_ID + ".npy", low_res_array)
+            
+            if debug_mode:
+                np.save("./Data/RS_Images/npy/" + file_ID + "-full_res.npy", full_res_array)
 
-        return depth_image
+        return full_res_array
 
 
     def save_RGB_image(self, file_ID):
@@ -64,7 +74,9 @@ class RS_Imager:
             Purpose: Saves an RGB image using the RealSense camera. Can optionally also save the image as an ndarray.
             Note: Assumes that these folders already exist:
                 ./Data/RS_Images/jpg-rgb
-                            
+            
+            Arg - file_ID (string): this is the name of the file(s) to be saved. Do NOT include the file extension.  
+            
             Returns: an ndarray corresponding to the rgb image
         '''
 
@@ -75,5 +87,3 @@ class RS_Imager:
         cv2.imwrite("./Data/RS_Images/rgb-jpg/" + file_ID + ".jpg", image)
 
         return image
-
-
--- use_rs_imaging.py
+++ use_rs_imaging.py
@@ -7,6 +7,7 @@ This is an example of how to use rs_imaging.py
 
 import rs_imaging
 from pathlib import Path
+import time
 
 Path("./Data/").mkdir(parents=True, exist_ok=True)
 Path("./Data/RS_Images/").mkdir(parents=True, exist_ok=True)
@@ -17,5 +18,7 @@ Path("./Data/RS_Images/rgb-jpg").mkdir(parents=True, exist_ok=True)
 
 Imager = rs_imaging.RS_Imager()
 
-Imager.save_depth_image()
-Imager.save_RGB_image()
+curr_time = time.strftime("%Y%m%d-%H%M%S")
+
+Imager.save_depth_image(curr_time, save_array=True)
+Imager.save_RGB_image(curr_time)
